e6f236a7bc25033e11c0a2850c6f408bfd84b216
GabrielDev-21 <gs2000131@gmail.com>
2021-11-21 19:58:22 -0300

    first commit

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php
+	define("CHARSET", "utf8");
+	define("NRLINHAS", 30);
+	define("SESSAOFIB", "FIBPORTARIA");
+	define("URL_BASE", "");
+	
+	date_default_timezone_set('America/Sao_Paulo');
+?>
---
+++ include/crud.php
@@ -0,0 +1,106 @@
+<?php
+	function open() {
+		//$conexao = @mysqli_connect(SERVIDOR, USUARIO, SENHA, BANCO) or die(mysqli_connect_error());
+		$envPath = realpath(dirname(__FILE__). '/env.ini');
+    	$env = parse_ini_file($envPath);
+    	$conexao = new mysqli($env['DB_HOST'],$env['DB_USER'],$env['DB_SENHA'],$env['DB_NAME']) or die(mysqli_connect_error());;
+		@mysqli_set_charset($conexao, CHARSET);
+		return $conexao;
+	}
+
+	function close($conexao) {
+		@mysqli_close($conexao) or die (@mysqli_error($conexao));
+	}
+
+	function execute ($sql, $id = false) {
+		$conexao = open();
+		$qry = @mysqli_query($conexao, $sql) or die(@mysqli_error($conexao));
+		
+		if ($id)
+			$qry = mysqli_insert_id($conexao);
+
+		close($conexao);
+		return $qry;
+	}
+
+	function select ($tabela, $condicao=null, $campos="*") {
+		if ($condicao != null) {
+			$condicao = " WHERE " . $condicao;
+		}
+		$sql = "SELECT {$campos} FROM {$tabela} {$condicao} ";
+
+		//var_dump($sql);
+
+		$qry = execute($sql);
+		if (!@mysqli_num_rows($qry)) {
+			return false;
+		} else {
+			while ($linha = @mysqli_fetch_array($qry)) {
+				$dados[] = $linha;
+			}
+			return $dados;
+		}
+	}
+
+	function selectsql ($sql) {
+		$qry = execute($sql);
+		if (!mysqli_num_rows($qry)) {
+			return false;
+		} else {
+			while ($linha = @mysqli_fetch_assoc($qry)) {
+				$dados[] = $linha;
+			}
+		return $dados;	
+		}
+	}
+
+	function insert ($tabela, array $dados, $id = false) {
+		$dados = escapa($dados);
+		$campos = implode(", ", array_keys($dados));
+		$valores = "'" . implode("', '", $dados) . "'";
+		$sql = "INSERT INTO $tabela ({$campos}) VALUES ({$valores}) ";
+		return execute($sql, $id);
+	}
+
+	function update ($tabela, array $dados, $condicao) {
+		$dados = escapa($dados);
+		foreach ($dados as $chave => $valor) {
+			$campos[] = "{$chave} = '{$valor}' ";
+		}	
+		$campos = implode(", ", $campos);
+		$sql = "UPDATE {$tabela} SET {$campos} WHERE {$condicao}";
+		return execute($sql);
+	}
+
+	function delete ($tabela, $condicao) {
+		$sql = "DELETE FROM {$tabela} WHERE {$condicao}";
+		return execute($sql);
+	}
+
+	function numrows ($tabela, $condicao=null) {
+		$sql = "SELECT * FROM {$tabela} {$condicao}";
+		//var_dump($sql);
+		$qry = execute($sql);
+		return mysqli_num_rows($qry);
+	}
+
+	function escapa($data) {
+		$link = open();
+		//var_dump($data);
+		if (!is_array($data))
+			$dados = mysqli_real_escape_string($link, $data);
+		else {
+			$arr = $data;
+
+			foreach ($arr as $key => $value) {
+				$key 	= mysqli_real_escape_string($link, $key);
+				$value 	= mysqli_real_escape_string($link, $value);
+
+				$data[$key] = $value; 
+			}
+		}
+		//var_dump($data);
+		close($link);
+		return $data;
+    }
+?>
