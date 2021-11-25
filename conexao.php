5e28739ba1ed6be5034a87de0688970edacff540
GabrielDev-21 <gs2000131@gmail.com>
2021-11-25 20:02:44 -0300

    alterar

--- conexao.php
+++ conexao.php
@@ -1,16 +1,14 @@
 <?php
-// define('HOST', '127.0.0.1');
-// define('USUARIO', 'root');
-// define('SENHA', '');
-// define('DB','fib');
+$server = "ec2-50-16-241-192.compute-1.amazonaws.com";
+$user = "cktohxctskgqym";
+$password = "e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74";
+$port = "5432";
 
-// $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Nao foi possivel conectar');
+$infos_string = "host=$server port=$port dbname=dbdn07u65ldit7 user=$user password=";
 
-define('HOST', 'ec2-50-16-241-192.compute-1.amazonaws.com');
-define('USUARIO', 'cktohxctskgqym');
-define('SENHA', 'e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74');
-define('DB','dbdn07u65ldit7');
+$conection = pg_connect($infos_string) or
+die ("NÃO FOI POSSIVEL CONECTAR AO SERVIDOR");
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Nao foi possivel conectar');
+?>
 
 ?>
