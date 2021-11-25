c303a91116c217a9b0966bc79f714c8df84f353a
GabrielDev-21 <gs2000131@gmail.com>
2021-11-25 19:45:45 -0300

    alterar

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-	define("SERVIDOR", "localhost");
-	define("USUARIO", "root");
-	define("SENHA", "");
-	define("BANCO", "fib");
+	define("SERVIDOR", "ec2-50-16-241-192.compute-1.amazonaws.com");
+	define("USUARIO", "cktohxctskgqym");
+	define("SENHA", "e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74");
+	define("BANCO", "dbdn07u65ldit7");
 	define("CHARSET", "utf8");
 	define("NRLINHAS", 30);
 	define("SESSAOFIB", "FIBPORTARIA");
