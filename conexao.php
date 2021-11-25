5c5670a34b48cdd815a29c6335d3d9cfc199bde7
GabrielDev-21 <gs2000131@gmail.com>
2021-11-25 20:35:35 -0300

    alterar

--- conexao.php
+++ conexao.php
@@ -1,13 +1,3 @@
 <?php
-$db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["ec2-50-16-241-192.compute-1.amazonaws.com"],
-    $db["5432"],
-    $db["cktohxctskgqym"],
-    $db["e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74"],
-    ltrim($db["path"], "/")
-));
-
+$conexao = new PDO("pgsql:host=ec2-50-16-241-192.compute-1.amazonaws.com;port=5432;dbname=dbdn07u65ldit7;user=cktohxctskgqym;password=e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74");
 ?>
