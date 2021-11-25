e41d5649187792e7e2d7db656450f4b30d99d043
GabrielDev-21 <gs2000131@gmail.com>
2021-11-25 20:47:21 -0300

    alterar

--- conexao.php
+++ conexao.php
@@ -1,3 +1,9 @@
 <?php
-$conexao = new PDO("pgsql:host=ec2-50-16-241-192.compute-1.amazonaws.com;port=5432;dbname=dbdn07u65ldit7;user=cktohxctskgqym;password=e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74");
+define('HOST', 'ec2-50-16-241-192.compute-1.amazonaws.com');
+define('USUARIO', 'cktohxctskgqym');
+define('SENHA', 'e53d0af2ea7c7688fec3909f104f64143b12e46a67d35a5e8a230c9236b36c74');
+define('DB','dbdn07u65ldit7');
+define('PORT', '5432');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB, PORT) or die ('Nao foi possivel conectar');
 ?>
